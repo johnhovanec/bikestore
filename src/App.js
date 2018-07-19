6447f734ee30066a602cf1b67806d0643a347a9e
johnhovanec <jahovanec@gmail.com>
2018-07-18 22:21:13 -0400

    Add react router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
 
@@ -20,11 +21,82 @@ class NewDiv extends Component {
   render() {
     return (
       <div>
-        <h1>Hello </h1>
+        <h1>Hello</h1>
       </div>
     );
   }
 }
+
+
+
+const BasicExample = () => (
+  <Router>
+    <div>
+      <App />
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/products">Products</Link>
+          </li>
+        </ul>
+
+      <hr />
+
+      <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/products" component={Products} />
+    </div>
+  </Router>
+);
+
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+  </div>
+);
+
+const About = () => (
+  <div>
+    <h2>About</h2>
+  </div>
+);
+
+const Products = ({ match }) => (
+  <div>
+    <h2>Products</h2>
+    <ul>
+      <li>
+        <Link to={`${match.url}/mountain`}>Mountain</Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/road`}>Road</Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/hybrid`}>Hybrid</Link>
+      </li>
+    </ul>
+
+    <Route path={`${match.url}/:productId`} component={Product} />
+    <Route
+      exact
+      path={match.url}
+      render={() => <h3>Please select a product type.</h3>}
+    />
+  </div>
+);
+
+const Product = ({ match }) => (
+  <div>
+    <h3>{match.params.productId}</h3>
+  </div>
+);
+
+export default BasicExample;
 
 class App extends Component {
   render() {
@@ -44,4 +116,4 @@ class App extends Component {
 }
 
 
-export default App;
+// export default App;
